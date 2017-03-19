6370d798904c4b04b956cd78db0bab21e1ba4d38
JBCDroid2 <jonathan.lyndo@outlook.com>
2017-03-19 14:46:22 -0300

    Build Venom into Settings
    And add Substratum dashboard tile

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -162,4 +162,8 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
+	
+    // Viper Additions.
+    public static class VenomSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class SubstratumActivity extends SettingsActivity { /* empty */ }
 }
