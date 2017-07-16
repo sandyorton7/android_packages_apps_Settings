a31272c36337941761f21bdd37053862c0f05805
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-07-15 23:58:51 -0300

    Build okhttp and okio
    http://square.github.io/okhttp/

--- Android.mk
+++ Android.mk
@@ -23,7 +23,9 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v14-preference \
     jsr305 \
     settings-logtags \
-    org.cyanogenmod.platform.internal
+    org.cyanogenmod.platform.internal \
+    okhttpcustom \
+    okio
 
 LOCAL_MODULE_TAGS := optional
 
@@ -59,6 +61,14 @@ include frameworks/opt/setupwizard/library/common-full-support.mk
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+    okhttpcustom:libs/okhttp-3.8.1.jar \
+    okio:libs/okio-1.13.0.jar
+
+include $(BUILD_MULTI_PREBUILT)
 
 # Use the following include to make our test apk.
 ifeq (,$(ONE_SHOT_MAKEFILE))
