ad443ddbc64db2658c5dc03e21814d62634a55a1
JBCDroid2 <jonathan.lyndo@outlook.com>
2018-11-06 18:05:30 -0200

    Add QS tile style doublecircle [2/3]

--- src/com/android/settings/display/QsTileStyles.java
+++ src/com/android/settings/display/QsTileStyles.java
@@ -91,6 +91,9 @@ public class QsTileStyles extends InstrumentedDialogFragment implements OnClickL
 
         LinearLayout teardrop = mView.findViewById(R.id.QsTileStyleTearDrop);
         setLayout("4", teardrop);
+
+        LinearLayout doublecircle = mView.findViewById(R.id.QsTileStyleDoubleCircle);
+        setLayout("5", doublecircle);
     }
 
     @Override
@@ -139,6 +142,7 @@ public class QsTileStyles extends InstrumentedDialogFragment implements OnClickL
         LinearLayout roundedsquare = mView.findViewById(R.id.QsTileStyleRoundedSquare);
         LinearLayout squircle = mView.findViewById(R.id.QsTileStyleSquircle);
         LinearLayout teardrop = mView.findViewById(R.id.QsTileStyleTearDrop);
+        LinearLayout doublecircle = mView.findViewById(R.id.QsTileStyleDoubleCircle);
 
         TypedValue typedValue = new TypedValue();
         res.getValue(R.dimen.qs_styles_layout_opacity, typedValue, true);
@@ -149,26 +153,37 @@ public class QsTileStyles extends InstrumentedDialogFragment implements OnClickL
             roundedsquare.setAlpha(mLayoutOpacity);
             squircle.setAlpha(mLayoutOpacity);
             teardrop.setAlpha(mLayoutOpacity);
+            doublecircle.setAlpha(mLayoutOpacity);
         } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 2 )) {
             square.setAlpha(mLayoutOpacity);
             roundedsquare.setAlpha((float) 1.0);
             squircle.setAlpha(mLayoutOpacity);
             teardrop.setAlpha(mLayoutOpacity);
+            doublecircle.setAlpha(mLayoutOpacity);
         } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 3 )) {
             square.setAlpha(mLayoutOpacity);
             roundedsquare.setAlpha(mLayoutOpacity);
             squircle.setAlpha((float) 1.0);
             teardrop.setAlpha(mLayoutOpacity);
+            doublecircle.setAlpha(mLayoutOpacity);
         } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 4 )) {
             square.setAlpha(mLayoutOpacity);
             roundedsquare.setAlpha(mLayoutOpacity);
             squircle.setAlpha(mLayoutOpacity);
             teardrop.setAlpha((float) 1.0);
+            doublecircle.setAlpha(mLayoutOpacity);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 5 )) {
+            square.setAlpha(mLayoutOpacity);
+            roundedsquare.setAlpha(mLayoutOpacity);
+            squircle.setAlpha(mLayoutOpacity);
+            teardrop.setAlpha(mLayoutOpacity);
+            doublecircle.setAlpha((float) 1.0);
         } else {
             square.setAlpha((float) 1.0);
             roundedsquare.setAlpha((float) 1.0);
             squircle.setAlpha((float) 1.0);
             teardrop.setAlpha((float) 1.0);
+            doublecircle.setAlpha((float) 1.0);
         }
     }
 }
